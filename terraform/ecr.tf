9fec067345483d44b5723f7c20e34b926450f446
Toki Horii <t-horii@giftee.co>
2022-09-20 23:58:29 +0900

    ecr設定

---
+++ terraform/ecr.tf
@@ -0,0 +1,8 @@
+resource "aws_ecr_repository" "repo" {
+  name                 = var.app_name
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
